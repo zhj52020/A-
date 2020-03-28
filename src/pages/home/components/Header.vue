1bf91beaa9514e916f7c9da08e408775ad10212e
张杰 <1640616848@qq.com>
2020-03-28 21:32:11 +0800

    三月28日日脚手架搭建

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="header">
+        <div class="content">
+            <img class="logo" src="~@/assets/common/header/logo.svg" alt="">
+            <a href="">
+                <i class=""></i>
+            </a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'HomeHeader',
+}
+</script>
+<style lang="stylus" scoped>
+.content
+    width:1163px;
+    height:46px;
+    line-height:46px;
+    .logo
+        width:92px;
+        height:46px;
+        margin-right:12px;
+.header
+    height:46px;
+    width:100%;
+</style>
